108de29fbbf024df296b1a332d1543d69a1091c0
AlenaSkurat <skuraten@gmail.com>
2017-09-28 00:20:18 +0300

    The zeros function has been implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function zeros(expression) {
-  // your solution
+  
+      var a = expression.split("*"),
+      result = 0,
+      two = 0,
+      five = 0,
+      count = 0,
+      temp = 0;
+  
+      for (var i = 0; i < a.length; i++) {
+          if(a[i].search("!!") > 0) {
+              result = a[i].replace("!!","");
+              while (result != 0 && result != 1) {
+                  temp = result;
+  
+                  while (temp%2 == 0) {
+                      two++;
+                      temp/=2;
+                  }
+                  while (temp%5 == 0) {
+                      five++;
+                      temp/=5;
+                  }
+                  result -= 2;
+              }
+          } else {
+              result = a[i].replace("!","");
+              while (result != 0 && result != 1) {
+                  temp = result;
+  
+                  while (temp%2 == 0) {
+                      two++;
+                      temp/=2;
+                  }
+                  while (temp%5 == 0) {
+                      five++;
+                      temp/=5;
+                  }
+                  result -= 1;
+              }
+              }
+      }
+      return Math.min(two,five);
 }
